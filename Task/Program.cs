bb288caa3c774c5b6e7640f87818ebccd7a58509
Albert <mona-m1@ya.ru>
2022-12-16 09:30:37 +0600

    end coding

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,11 +4,8 @@
 string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
 
-
-
-
-
-
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
 
 
 void SecondArrayWithIF(string[] array1, string[] array2)
