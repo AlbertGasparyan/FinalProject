b75427cfbfe2fe2189adb98ddd024b6b941dc711
Albert <mona-m1@ya.ru>
2022-12-16 09:27:17 +0600

    one methode

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿System.Console.Clear();
+
+
+
+
+
+
+
+
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
